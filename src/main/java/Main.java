c1774fef15537a219eb094c5be4d81468ee7806c
Abbey Van De Vorst <abbey@canva.com>
2024-04-27 16:13:51 +1000

    Respond with 200

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,24 +1,34 @@
+import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 public class Main {
   public static void main(String[] args) {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     System.out.println("Logs from your program will appear here!");
 
-     //Uncomment this block to pass the first stage
+     try (var serverSocket = new ServerSocket(4221)){
+         serverSocket.setReuseAddress(true);
+         var clientSocket = serverSocket.accept(); // Wait for connection from client.
 
-     ServerSocket serverSocket = null;
-     Socket clientSocket = null;
+//         var inStream = clientSocket.getInputStream();
+//         var in = inStream.readAllBytes();
 
-     try {
-       serverSocket = new ServerSocket(4221);
-       serverSocket.setReuseAddress(true);
-       clientSocket = serverSocket.accept(); // Wait for connection from client.
-       System.out.println("accepted new connection");
+         var outStream = clientSocket.getOutputStream();
+         outStream.write(constructOkResponse().getBytes(StandardCharsets.UTF_8));
+
+//         inStream.close();
+         outStream.close();
+
+         System.out.println("accepted new connection");
      } catch (IOException e) {
        System.out.println("IOException: " + e.getMessage());
      }
+  }
+
+  private static String constructOkResponse() {
+      return "HTTP/1.1 200 OK\r\n\r\n";
   }
 }
