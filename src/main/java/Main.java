aed1d3c014331e58eb9adc6964f05239a9c545cf
Abbey Van De Vorst <abbey@canva.com>
2024-04-28 12:20:06 +1000

    Little refactor

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,7 @@
 import java.io.*;
 import java.net.ServerSocket;
-import java.net.Socket;
-import java.nio.charset.StandardCharsets;
-import java.util.Map;
 
 public class Main {
-
-    private static final int MAX_LINES = 10;
-
-    private static class HttpRequest {
-        String method;
-        String path;
-        String version;
-        Map<String, String> headers;
-    }
 
   public static void main(String[] args) {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -21,10 +9,11 @@ public class Main {
 
      try (var serverSocket = new ServerSocket(4221)){
          serverSocket.setReuseAddress(true);
-         var clientSocket = serverSocket.accept(); // Wait for connection from client.
-         System.out.println("accepted new connection");
-         var connection = new HttpConnection(clientSocket);
-         connection.handleRequest();
+         try (var clientSocket = serverSocket.accept()) { // Wait for connection from client.
+             System.out.println("accepted new connection");
+             var connection = new HttpConnection(clientSocket);
+             connection.handleRequest();
+         }
      } catch (IOException e) {
        System.out.println("IOException: " + e.getMessage());
      }
